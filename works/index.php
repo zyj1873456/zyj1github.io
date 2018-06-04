d485f71745686dd2fb1baabd7f86ed6c7b9a8902
zyj1873456 <39907589+zyj1873456@users.noreply.github.com>
2018-06-04 21:32:59 +0800

    Add files via upload

---
+++ works/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>欢迎来到作业提交系统！</title>
+<style>
+body{
+	background-color: #C4E1FF;
+}
+.box3{
+    margin-left: 20px;
+    background-color: #D2E9FF;
+    width:96%;
+    height:700px;
+    border-bottom-width:1px;
+    border-bottom-style: solid;
+    border-bottom-color:#999;
+    border-left-width: 1px;
+    border-left-style: solid;
+    border-left-color:#999;
+    border-top-width:1px;
+    border-top-style:solid;
+    border-top-color:#999;
+    border-right-width:1px;
+    border-right-style: solid;
+    border-right-color:#999;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div class="box3">
+    <div>
+      <p align="center">  <img src=".\img\head.png" align="center" width="100%" height=200px></img></p>
+    </div>
+    	<h1 align="center">请选择您的身份：</h1>
+    <div >
+    	<p align="center"><a href="./login/login.php">学生</a></p>
+        <p align="center"><a href="./login/login_te.php">老师</a></p>
+        <p align="center"><a href="./login/login_ad.php">管理员</a></p>
+    </div>
+</div>    
+</body>
+</html>
